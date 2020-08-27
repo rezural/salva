bb8fa6292f2b1c58d65a059007828c08b68f6358
Nick Hemsley <nick.hems@gmail.com>
2020-08-27 22:05:41 +1000

    wavepool

--- examples3d/helper.rs
+++ examples3d/helper.rs
@@ -18,3 +18,29 @@ pub fn cube_fluid(ni: usize, nj: usize, nk: usize, particle_rad: f32, density: f
 
     Fluid::new(points, particle_rad, density)
 }
+
+pub fn wavepool(width: f32, height: f32, depth: f32, particle_rad: f32, density: f32) -> Fluid<f32> {
+    let particle_diam = particle_rad * 2.0;
+    let size_x = (width / particle_diam).ceil() as i32;
+    let size_y = (height / particle_diam).ceil() as i32;
+    let size_z = (depth / particle_diam).ceil() as i32;
+
+    let mut points = Vec::new();
+
+    let num_particles = size_x * size_y * size_z;
+    for i in 0..size_x {
+        for j in 0..size_y {
+            for k in 0..size_z {
+                let x = (i as f32) * particle_diam;
+                let y = (j as f32) * particle_diam;
+                let z = (k as f32) * particle_diam;
+                points.push(Point3::new(x, y, z) + Vector3::repeat(particle_rad) - half_extents);
+            }
+        }
+    }
+    // let half_extents = Vector3::new(ni as f32, nj as f32, nk as f32) * particle_rad;
+    // Fluid::new(points, particle_rad, density)
+    Fluid::new(points, particle_rad, density)
+
+    
+}
---
+++ examples3d/wavepool.rs
@@ -0,0 +1,162 @@
+extern crate nalgebra as na;
+
+use na::{Isometry3, Point3, Vector3};
+use ncollide3d::shape::{Cuboid, ShapeHandle};
+use nphysics3d::force_generator::DefaultForceGeneratorSet;
+use nphysics3d::joint::DefaultJointConstraintSet;
+use nphysics3d::object::{
+    BodyPartHandle, ColliderDesc, DefaultBodySet, DefaultColliderSet, Ground,
+};
+use nphysics3d::world::{DefaultGeometricalWorld, DefaultMechanicalWorld};
+use nphysics_testbed3d::objects::FluidRenderingMode;
+use nphysics_testbed3d::Testbed;
+use salva3d::coupling::{ColliderCouplingSet, CouplingMethod};
+use salva3d::object::{Boundary, Fluid};
+use salva3d::solver::{ArtificialViscosity, DFSPHSolver};
+use salva3d::LiquidWorld;
+use std::f32;
+
+#[path = "./helper.rs"]
+mod helper;
+
+pub fn init_world(testbed: &mut Testbed) {
+    /*
+     * World
+     */
+    let mechanical_world = DefaultMechanicalWorld::new(Vector3::new(0.0, -9.81, 0.0));
+    let geometrical_world = DefaultGeometricalWorld::new();
+    let mut bodies = DefaultBodySet::new();
+    let mut colliders = DefaultColliderSet::new();
+    let joint_constraints = DefaultJointConstraintSet::new();
+    let force_generators = DefaultForceGeneratorSet::new();
+
+    // Parameters of the ground.
+    let ground_thickness = 0.2;
+    let ground_half_width = 1.5;
+    let ground_half_height = 0.7;
+
+    /*
+     * Liquid world.
+     */
+    let particle_rad = 0.01;
+    let solver: DFSPHSolver<f32> = DFSPHSolver::new();
+    let mut liquid_world = LiquidWorld::new(solver, particle_rad, 2.0);
+    let mut coupling_manager = ColliderCouplingSet::new();
+
+    // Liquid.
+    let nparticles = 50;
+    let mut fluid: Fluid<f32> = helper::cube_fluid(nparticles, nparticles, nparticles, particle_rad, 3000.0);
+    fluid.transform_by(&Isometry3::translation(
+        0.4,
+        ground_thickness + nparticles as f32 * particle_rad,
+        0.0,
+    ));
+
+    println!("size: {}", fluid.size_in_bytes());
+
+    let viscosity = ArtificialViscosity::new(1.0, 0.0);
+    fluid.nonpressure_forces.push(Box::new(viscosity));
+    let fluid_handle = liquid_world.add_fluid(fluid);
+    testbed.set_fluid_color(fluid_handle, Point3::new(0.8, 0.7, 1.0));
+
+    /*
+     * Ground.
+     */
+    let ground_shape = ShapeHandle::new(Cuboid::new(Vector3::new(
+        ground_half_width,
+        ground_thickness,
+        ground_half_width,
+    )));
+    let wall_shape = ShapeHandle::new(Cuboid::new(Vector3::new(
+        ground_thickness,
+        ground_half_height,
+        ground_half_width,
+    )));
+
+    let ground_handle = bodies.insert(Ground::new());
+
+    let wall_poses = [
+        Isometry3::new(
+            Vector3::new(0.0, ground_half_height, ground_half_width),
+            Vector3::y() * (f32::consts::PI / 2.0),
+        ),
+        Isometry3::new(
+            Vector3::new(0.0, ground_half_height, -ground_half_width),
+            Vector3::y() * (f32::consts::PI / 2.0),
+        ),
+        Isometry3::translation(ground_half_width, ground_half_height, 0.0),
+        Isometry3::translation(-ground_half_width, ground_half_height, 0.0),
+    ];
+
+    for pose in wall_poses.iter() {
+        let co = ColliderDesc::new(wall_shape.clone())
+            .position(*pose)
+            .build(BodyPartHandle(ground_handle, 0));
+        let co_handle = colliders.insert(co);
+        let bo_handle = liquid_world.add_boundary(Boundary::new(Vec::new()));
+
+        coupling_manager.register_coupling(
+            bo_handle,
+            co_handle,
+            CouplingMethod::DynamicContactSampling,
+        );
+    }
+
+    let co = ColliderDesc::new(ground_shape).build(BodyPartHandle(ground_handle, 0));
+    let co_handle = colliders.insert(co);
+    let bo_handle = liquid_world.add_boundary(Boundary::new(Vec::new()));
+
+    coupling_manager.register_coupling(
+        bo_handle,
+        co_handle,
+        CouplingMethod::DynamicContactSampling,
+    );
+
+    /*
+     * Set up the testbed.
+     */
+    testbed.set_body_wireframe(ground_handle, true);
+    testbed.set_ground_handle(Some(ground_handle));
+
+    testbed.set_world(
+        mechanical_world,
+        geometrical_world,
+        bodies,
+        colliders,
+        joint_constraints,
+        force_generators,
+    );
+    testbed.set_liquid_world(liquid_world, coupling_manager);
+    testbed.set_fluid_rendering_mode(FluidRenderingMode::VelocityColor { min: 0.0, max: 5.0 });
+    testbed.mechanical_world_mut().set_timestep(1.0 / 200.0);
+    testbed.look_at(Point3::new(3.0, 3.0, 3.0), Point3::origin());
+}
+
+fn main() {
+    let testbed = Testbed::from_builders(0, vec![("Boxes", init_world)]);
+    testbed.run()
+}
+
+#[cfg(test)]
+
+//  use super::*;
+// // use super::*;
+
+// extern crate rand;
+// use rand::{thread_rng, Rng};
+// use rand::distributions::Alphanumeric;
+
+#[test]
+
+// fn dump_sizes() {
+//     let particle_radius: f32 = 0.1;
+//     let particle_diam = particle_radius * 2;
+
+//     sizes = [(10.0 , 1.0),
+//              (20.0 , 3.0),
+//              (100.0, 5.0)];
+    
+//     slicings = sizes
+//                 .into_iter()
+
+// }
