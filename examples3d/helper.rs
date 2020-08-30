47adb48f6d6cfa2a25170eb4607a4c3b5dab0e27
rezural <rezural@protonmail.com>
2020-08-30 18:25:36 +1000

    Merge conflict

--- examples3d/helper.rs
+++ examples3d/helper.rs
@@ -19,7 +19,7 @@ pub fn cube_fluid(ni: usize, nj: usize, nk: usize, particle_rad: f32, density: f
     Fluid::new(points, particle_rad, density)
 }
 
-pub fn volume_of_liquid(width: f32, height: f32, depth: f32, particle_rad: f32, density: f32) -> Fluid<f32> {
+pub fn box_of_particles(width: f32, height: f32, depth: f32, particle_rad: f32, translation: Vector3<f32>) -> Vec<Point3<f32>> {
     let particle_diam = particle_rad * 2.0;
     let size_x = (width / particle_diam).ceil() as i32;
     let size_y = (height / particle_diam).ceil() as i32;
@@ -29,18 +29,32 @@ pub fn volume_of_liquid(width: f32, height: f32, depth: f32, particle_rad: f32, 
 
     let mut points = Vec::new();
 
-    for i in 0..size_x {
-        for j in 0..size_y {
+    let mut offset = false;
+    for i in 0..size_y {
+
+        let row_translation = match offset {
+            true => Vector3::new(-particle_rad/2.0, 0.0, -particle_rad/2.0),
+            false => Vector3::new(particle_rad/2.0, 0.0, particle_rad/2.0)
+        };
+
+        for j in 0..size_x {
             for k in 0..size_z {
-                let x = (i as f32) * particle_diam;
-                let y = (j as f32) * particle_diam;
+                let x = (j as f32) * particle_diam;
+                let y = (i as f32) * particle_diam;
                 let z = (k as f32) * particle_diam;
-                points.push(Point3::new(x, y, z) + Vector3::repeat(particle_rad) - half_extents);
+                points.push((Point3::new(x, y, z) + Vector3::repeat(particle_rad) - half_extents) + row_translation + translation);
 
             }
         }
+        offset = !offset;
     }
+
+    points
+}
+
+pub fn box_of_fluid(width: f32, height: f32, depth: f32, particle_radius: f32, density: f32) -> Fluid<f32> {
+
     // let half_extents = Vector3::new(ni as f32, nj as f32, nk as f32) * particle_rad;
     // Fluid::new(points, particle_rad, density)
-    Fluid::new(points, particle_rad, density)
+    Fluid::new(box_of_particles(width, height, depth, particle_radius, Vector3::new(0.0, 0.0, 0.0)), particle_radius, density)
 }
--- examples3d/wavepool.rs
+++ examples3d/wavepool.rs
@@ -26,9 +26,11 @@ mod helper;
 pub fn init_world(testbed: &mut Testbed) {
 
     // Fluid sim world dimensions
-    let (width, height, depth, particle_radius) = (100.0, 15.0, 100.0, 1.0 / 3.0);
+    let (width, height, depth, particle_radius) = (100.0, 15.0, 100.0, 1.0 / 4.0);
 
     let subdivs = ((width/particle_radius) as i32, (height/particle_radius) as i32, (depth/particle_radius) as i32);
+
+    let fluid_density: f32 = 1000.0;
 
     println!("{{\"width\": {}, \"height\": {}, \"depth\": {},\"particle_radius\": {},\"subdivs\": [{},{},{}]}}",
         width, height, depth, particle_radius, subdivs.0, subdivs.1, subdivs.2);
@@ -50,22 +52,15 @@ pub fn init_world(testbed: &mut Testbed) {
     // This needs to be slightly larger than half the width
     let ground_half_width: f32 = width * 0.53;
     let ground_half_height: f32 = height;
-    // println!("DT: {}, {}", mechanical_world.integration_parameters.dt(), mechanical_world.integration_parameters.inv_dt());
-    // mechanical_world.integration_parameters.set_dt(mechanical_world.integration_parameters.dt() / 100.0);
-    // println!("DT: {}, {}", mechanical_world.integration_parameters.dt(), mechanical_world.integration_parameters.inv_dt());
 
     /*
      * Liquid world.
      */
-    // let particle_rad = 0.01;
     let solver: DFSPHSolver<f32> = DFSPHSolver::new();
     let mut liquid_world = LiquidWorld::new(solver, particle_radius, 2.0);
     let mut coupling_manager = ColliderCouplingSet::new();
 
-    // Liquid.
-    // let mut fluid: Fluid<f32> = helper::cube_fluid(nparticles, nparticles, nparticles, particle_radius, 3000.0);
-    // println!("wavepool");
-    let mut fluid: Fluid<f32> = helper::volume_of_liquid(width, height, depth, particle_radius, 1000.0);
+    let mut fluid: Fluid<f32> = helper::box_of_fluid(width, height, depth, particle_radius, fluid_density);
 
     fluid.transform_by(&Isometry3::translation(
         0.0,
@@ -145,44 +140,45 @@ pub fn init_world(testbed: &mut Testbed) {
     // Callback that will be executed on the main loop to generate new particles every second.
     let mut last_t = 0.0;
     let mut simulation_step = 0;
+    let mut done_first = false;
 
     testbed.add_callback_with_fluids(move |liquid_world: & mut LiquidWorld<f32>, _, _, _, _, _, _, t| {
         // println!("{:?}", liquid_world.fluids().values().next().unwrap().positions.len());
-        let state_save_path = path::PathBuf::from(String::from("./runs/test"));
-        save_state(&state_save_path, liquid_world, simulation_step);
+        // let sttate_save_path = path::PathBuf::from(String::from("./runs/test"));
+        // save_state(&state_save_path, liquid_world, simulation_step);
+        // println!("here");
         simulation_step = simulation_step + 1;
         
         let fluid = liquid_world.fluids_mut().get_mut(fluid_handle).unwrap();
 
-        for i in 0..fluid.num_particles() {
-            if fluid.positions[i].y < -2.0 {
-                fluid.delete_particle_at_next_timestep(i);
-            }
-        }
-
-        if t - last_t < 2.0 {
+        println!("{}", t);
+        if !done_first {
+          if t > 0.1 {
+              done_first = true;
+          } else {
+              return;
+          }
+        } else if t - last_t < 4.0 {
             return;
         }
+        
+        
 
         last_t = t;
-        let diam = particle_radius * 2.0;
-        let nparticles = 8;
-        let mut particles = Vec::new();
-        let mut velocities = Vec::new();
-        let shift = 30.0;
-        let vel = 3.0;
 
-        for i in 0..nparticles {
-            for j in 0..nparticles * 2 {
-                for k in 0..nparticles {
-                    let pos = Point3::new(i as f32 * diam, j as f32 * diam, k as f32 * diam);
-                    particles.push(pos + Vector3::new(0.0, shift, 0.0));
-                    velocities.push(Vector3::y() * vel);
-                }
-            }
+        // let shift = Vector3::new(-(width - width / 6.0) , height + particle_radius, 0.0);
+        let shift = Vector3::new( - (width / 2.5 ), height * 2.3, 0.0);
+
+        let particles = &helper::box_of_particles(width / 6.0, height / 2.0, depth, particle_radius,shift);
+
+        let initial_particle_speed = 0.0;
+
+        let mut velocities = Vec::new();
+        for _ in 0..particles.to_vec().len() {
+            velocities.push(Vector3::y() * initial_particle_speed)
         }
 
-        fluid.add_particles(&particles, Some(&velocities));
+        fluid.add_particles(particles, Some(&velocities));
     });
 
     /*
@@ -205,6 +201,7 @@ pub fn init_world(testbed: &mut Testbed) {
     testbed.mechanical_world_mut().set_timestep(1.0 / 200.0);
     testbed.look_at(Point3::new(120.0, 50.0, 120.0), Point3::origin());
 }
+
 
 fn save_state(container_dir: &std::path::PathBuf, fluid: &LiquidWorld<f32>, simulation_step: u64) {
 
